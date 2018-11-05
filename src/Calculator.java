1716414279075639217249fb379001e3d10b306e
Manvir Chahal <Manvir.Chahal@mail.bcu.ac.uk>
2018-11-05 12:33:30 +0000

    Lab work (pair progamming)

---
+++ src/Calculator.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class Calculator {
+
+	public void loadConstant(int i) {
+		Stack<Integer> mystack=new Stack<Integer>();
+		
+		Calculator(){ 
+		
+	}
+
+	public void add() {
+		ArrayList<Integer> Calculator = new ArrayList<String>;
+		
+		Calculator.add(5);
+		Calculator.add(4);
+		
+	}
+
+	public void subtract() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void multiply() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void divide() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void total() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
